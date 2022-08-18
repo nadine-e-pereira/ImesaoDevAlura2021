6f6343a7ee32d3fd0ee1f86fa6674586c440c1ec
Nadine Pereira <81249310+nadine-e-pereira@users.noreply.github.com>
2022-08-18 10:23:55 -0300

    Create scripts.js

---
+++ Aula04/scripts.js
@@ -0,0 +1,40 @@
+//var filme1 =
+//var filme2 =
+//var filme3 =
+//console.log(filme1)
+//console.log(filme2)
+//console.log(filme3)
+
+// [] = array
+
+var filmes = ["StarWars", "ToyStory", "Interestellar"]
+
+//while (tentativas >0){
+//  tentativas = tentativas - 1
+//}
+
+//for(var i = 0 ; i <= 2; i++){
+  //console.log(filmes[i])
+//}
+
+for(var i = 0; i < filmes.length; i++){
+  console.log(filmes[i])
+}
+
+// i = uma variável de iteração
+//i= i - 1 = i--
+//i= i + 1 = i ++
+// O For compacta o While.
+
+//filmes.push("StarWars")
+//filmes.push("ToyStory")
+//filmes.push("Interestellar")
+
+//console.log(filmes[0])
+//console.log(filmes[1])
+//console.log(filmes[2])
+
+//Nos Arrays, os índices/ID's (Identificadores) começam no zero. O tamanho do Array é a sua quantidade de elementos. Array é um padrão da maioria das linguagens da programação. 
+//O for faz laços como o while, ele vai percoreer ou iterar a array.
+
+//console.log(filmes.length)
